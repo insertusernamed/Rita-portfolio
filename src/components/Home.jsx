117c67d1cd75e4089a97359015ccaebfa7c82042
Jenna <jennadeamer@gmail.com>
2025-01-25 16:59:14 -0500

    Refactor Home component structure and update Navbar links; adjust CSS for improved layout and styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,22 +2,24 @@ import "../styles/Home.css";
 
 function Home() {
     return (
-        <section className="home">
-            <h1 className="intro">Hey, I’m Rita!</h1>
-            <h2>
-                Engineer by day, creative problem-solver always. Dive into my
-                portfolio and see how I turn ideas into solutions.
-            </h2>
-            <p>
-                Automation and Software Engineering student adept at coding and
-                development of electromechanical systems. Experienced with
-                project optimization through time management and innovative
-                problem-solving. Skilled with troubleshooting and data-analysis
-                through Python, Java, and C/C. Skilled in implementing Microsoft
-                applications and Linux repositories.
-            </p>
-            <div className="button-container">
-                <a href="">View Work</a> <a href=""></a>
+        <section id="home-section">
+            <div className="hero-container">
+                <h1 className="intro">Hey, I’m Rita!</h1>
+                <h2>
+                    Engineer by day, creative problem-solver always. Dive into
+                    my portfolio and see how I turn ideas into solutions.
+                </h2>
+                <p>
+                    Automation and Software Engineering student adept at coding
+                    and development of electromechanical systems. Experienced
+                    with project optimization through time management and
+                    innovative problem-solving. Skilled with troubleshooting and
+                    data-analysis through Python, Java, and C/C. Skilled in
+                    implementing Microsoft applications and Linux repositories.
+                </p>
+                <div className="button-container">
+                    <a href="">View Work</a> <a href=""></a>
+                </div>
             </div>
         </section>
     );
--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -20,12 +20,12 @@ function Nav() {
             <div className="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul className="navbar-nav ms-2">
                     <li className="nav-item">
-                        <a className="nav-link" href="#projects">
+                        <a className="nav-link" href="#skills">
                             Skills
                         </a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#contact">
+                        <a className="nav-link" href="#projects">
                             Projects
                         </a>
                     </li>
